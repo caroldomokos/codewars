51027bad34890b8c4ee088bb66abefb2dc45cb5c
caroldomokos <carol.domokos@gmail.com>
2016-09-09 16:18:46 +0200

    Update Sum of the first nth term of Series.clj

--- Sum of the first nth term of Series.clj
+++ Sum of the first nth term of Series.clj
@@ -1,3 +1,9 @@
-(defn SeriesSum
-[number]
-(reduce #(
+(defn series-sum
+[n]
+(format "%.2f" (float (reduce + (map (fn [ranger] (/ 1 ranger)) (take n (iterate #(+ 3 %) 1)))))))
+;;; the code above passses the tests but times out...So I need to make it faster
+
+(defn series-sum [n]
+  (format "%.2f" (float (reduce +(take n (map #(/ 1 %) (iterate #(+ 3 %) 1)))))) 
+)
+;;; also times out - same overall logic
